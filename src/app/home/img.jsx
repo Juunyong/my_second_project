ba8f6136829855337f4620fba4329f5ea2cbbd35
Juunyong <qkrwnsdyd212@gmail.com>
2025-01-02 17:54:16 +0900

    0102fin

--- src/app/home/img.jsx
+++ src/app/home/img.jsx
@@ -1,14 +1,19 @@
 import { Image } from '@chakra-ui/next-js';
+import { Heading } from '@chakra-ui/react';
 import React from 'react';
 
 const Img = () => {
     return (
         <div className="h-screen bg-black ">
-            <div className="container flex flex-row gap-20 justify-center">
-                <Image src="/images/pattern/main/banksy1.jpg" alt="banksy1" width={500} height={500}></Image>
-                <Image src="/images/pattern/main/banksy2.jfif" alt="banksy1" width={500} height={500}></Image>
-                <Image src="/images/pattern/main/banksy4.jfif" alt="banksy1" width={500} height={500}></Image>
+            <Heading className="text-center text-[10vw] text-white">Imagination.</Heading>
+            <div className="container flex flex-row gap-20 justify-center py-20">
+                <Image src="/images/pattern/main/banksy1.jpg" alt="banksy1" width={400} height={400}></Image>
+                <Image src="/images/pattern/main/banksy_parking.webp" alt="banksy2" width={400} height={400}></Image>
+                <Image src="/images/pattern/main/flower_bomber.jpg" alt="banksy3" width={400} height={400}></Image>
             </div>
+            <Heading className="text-center text-[2vw] text-white">
+                Think outside the box, collapse the box, and take a fuxxing sharp knife to it.
+            </Heading>
         </div>
     );
 };
